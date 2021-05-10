56aa5165851448a67bf32c33885b510ad57b5436
Cadenshae <61713744+Cadenshae@users.noreply.github.com>
2021-05-10 13:52:58 +1200

    Create sw2.js

---
+++ sw2.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open('cadenshae-store').then((cache) => cache.addAll([
+            '/pwatest.html',
+            '/android-chrome-192x192.png',
+            '/android-chrome-512x512.png',
+            '/apple-touch-icon.png',
+            '/maskable_icon.png'
+        ])),
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    //console.log(e.request.url);
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request)),
+    );
+});
